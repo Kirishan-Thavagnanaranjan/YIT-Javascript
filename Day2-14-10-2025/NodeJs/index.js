f1c09ed4db6e66d4be84d0c51a01333ee55d03b8
Kirishan-Thavagnanaranjan <Thavagnanaranjankirishan@gmail.com>
2025-10-14 09:18:46 +0530

    Add examples of JavaScript function types
    Introduces various function definitions in JavaScript, including default, anonymous, arrow, parameterized, returning, and default value functions. Demonstrates usage and invocation of each function type for educational purposes.

---
+++ Day2-14-10-2025/NodeJs/index.js
@@ -0,0 +1,73 @@
+//Default function
+function myName(){
+    console.log("My name is Kirishan");
+};
+
+
+//Anonymous default function
+let myName1 = function(){
+    console.log("My name is Kirishan");
+};
+
+//Arrow default function
+let myName8 = () => {console.log("my name is kirishhan")};
+
+//parameterized function
+function myName2(name){
+    console.log("My name is " + name);
+}
+
+//parameterized anonymous function
+let myName3 =function(name){
+    console.log("My name is "+name);
+}
+
+//Parameterized arrow function
+let myName9 = (name) => {console.log("my name is "+name)}
+
+//returning function
+function myName4(name){
+    return "my name is" +name;
+}
+
+//returning anonymous function
+let myName5 =function(name){
+    return "my name is "+name;
+}
+
+//returning arrow function
+let myName10 = (name) => {return "My name is "+name};
+
+//default value function
+function myName6(name = "Kirishan"){
+    console.log("My name is " + name);
+}
+
+//default value anonymous function
+let myName7 = function(name="Kirishan"){
+    console.log("My name is " + name);
+}
+
+//default value arrow function
+let myName11 =(name ="kirishan") => {console.log("My name is "+name);}
+
+//arrow function with out return keyword
+let myName12 = (name) => "my name is " +name;
+
+let myName13 = (x,y) => x>y ? x:y;
+
+//calling functions
+myName();
+myName1();
+myName2("Kirishan");
+myName3("Kirishan");
+console.log(myName4("Kirishan"));
+console.log(myName5("Kirishan"));
+myName6();
+myName7("kirish");
+myName8();
+myName9("Kirishan");
+console.log(myName10("Kirishan"));
+myName11();
+console.log(myName12("Kirishannn"));
+console.log(myName13(10,5));
