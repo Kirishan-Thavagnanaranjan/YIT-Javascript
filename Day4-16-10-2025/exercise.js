9448863212db9fec48b524637bc74c0fe7d227d2
Kirishan-Thavagnanaranjan <Thavagnanaranjankirishan@gmail.com>
2025-10-17 23:27:20 +0530

    Fix class name and add multiple student instances
    Corrected the class name from Student to Students and updated constructor parameter naming. Added multiple Students instances and created an array to store them. Removed incorrect usage of foreach and started implementing iteration over the students array.

--- Day4-16-10-2025/exercise.js
+++ Day4-16-10-2025/exercise.js
@@ -3,13 +3,13 @@ class Students{
         this.fname=fname;
         this.lname=lname;
         this.grade=grade;
-        this.subjects=subject;
+        this.subjects=subjects;
         this.address=address;
         this.age=age;
     }
 
         getFullName(){
-        return this.fname +" "+ this.lname;
+            return this.fname +" "+ this.lname;
         }
 
     
@@ -19,13 +19,18 @@ class Students{
 
 
 
-let stu1 = new Student("Kirishan","Rajan","11D",["Maths","Science","Tamil"],"Jaffna",23);
-console.log(typeof Student);
-console.log(typeof stu1);
-console.log(stu1.getFullName());
+let stu1 = new Students("Kirishan","Rajan","11D",["Maths","Science","Tamil"],"Jaffna",23);
+let stu2 = new Students("Selvan","lingam","11E",["English","Science","Drama"],"KKS",23);
+let stu3 = new Students("Tanushan","sinnarasa","11D",["Hindu","Science","maths"],"Urumpirai",23);
+let stu4 = new Students("Viturshan","sangar","11D",["Maths","Science","Tamil"],"Chavakarceri",23);
+let stu5 = new Students("Arooran","sundar","11D",["Maths","Agriculture","Tamil"],"Kodikamam",23);
+
+let students = [stu1,stu2,stu3,stu4,stu5];
 
 
 
- Students.foreach(student => {
-    console.log(`{Students.fname}+{}`)
- })
+student.forEach(obj => {
+    if 
+})
+
+
