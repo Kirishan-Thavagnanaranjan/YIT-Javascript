e688aae9967125fdcaf248ff0e50ff9e22110415
Kirishan-Thavagnanaranjan <Thavagnanaranjankirishan@gmail.com>
2025-10-21 15:36:15 +0530

    Refactor Students class to use DOB and calculate age
    Replaced the 'age' property with 'dob' in the Students class and constructor. Added a getAge() method to calculate age from date of birth. Updated student instances to use DOB strings and modified tableData mapping to include calculated age and full name.

--- Day4-16-10-2025/exercise.js
+++ Day4-16-10-2025/exercise.js
@@ -1,11 +1,11 @@
 class Students{
-    constructor(fname,lname,grade,subjects,address,age){
+    constructor(fname,lname,grade,subjects,address,dob){
         this.fname=fname;
         this.lname=lname;
         this.grade=grade;
         this.subjects=subjects;
         this.address=address;
-        this.age=age;
+        this.dob=dob;
     }
 
         getFullName(){
@@ -13,17 +13,27 @@ class Students{
         }
 
     
+        getAge(){
+            const birthDate = new Date(this.dob);
+            const today = new Date();
+            let age = today.getFullYear() - birthDate.getFullYear();
+            const monthDiff = today.getMonth() - birthDate.getMonth();
+            if(monthDiff<0 || (monthDiff===0 && today.getDate() < birthDate.getDate())){
+                age--;
+            }
+            return age;
+        }
 
 }
 
 
 
 
-let stu1 = new Students("Kirishan","Rajan","11D",["Maths","Science","Tamil"],"Jaffna",23);
-let stu2 = new Students("Selvan","lingam","11E",["English","Science","Drama"],"KKS",23);
-let stu3 = new Students("Tanushan","sinnarasa","11D",["Hindu","Science","maths"],"Urumpirai",23);
-let stu4 = new Students("Viturshan","sangar","11D",["Maths","Science","Tamil"],"Chavakarceri",23);
-let stu5 = new Students("Arooran","sundar","11D",["Maths","Agriculture","Tamil"],"Kodikamam",23);
+let stu1 = new Students("Kirishan","Rajan","11D",["Maths","Science","Tamil"],"Jaffna","20-12-2002");
+let stu2 = new Students("Selvan","lingam","11E",["English","Science","Drama"],"KKS","8-8-2002");
+let stu3 = new Students("Tanushan","sinnarasa","11D",["Hindu","Science","maths"],"Urumpirai","7-5-2002");
+let stu4 = new Students("Viturshan","sangar","11D",["Maths","Science","Tamil"],"Chavakarceri","23-5-2002");
+let stu5 = new Students("Arooran","sundar","11D",["Maths","Agriculture","Tamil"],"Kodikamam","9-6-2002");
 
 let students = [stu1,stu2,stu3,stu4,stu5];
 
@@ -31,11 +41,17 @@ const tableData = students.map(student =>{
     const data={};
 
     for (let key in student){
-        if(Array.isArray(studnet[key])){
+        if(Array.isArray(student[key])){
             data[key] = student[key].join(",");
         }
         else{
             data[key] =student[key];
         }
     }
+    data.fullName = student.getFullName();
+    data.age = student.getAge();
+
+    return data;
 })
+
+console.table(tableData);
